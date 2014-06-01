c06a7768d61de658ee58afc3a9c51e0256f07225
Trey Dockendorf <treydock@gmail.com>
2014-05-31 19:44:08 -0500

    Update acceptance tests

--- spec/acceptance/infiniband_spec.rb
+++ spec/acceptance/infiniband_spec.rb
@@ -46,66 +46,6 @@ describe 'infiniband class' do
       'srptools',
     ].each do |pkg|
       describe package(pkg) do
-        it { should_not be_installed }
-      end
-    end
-
-    describe service('rdma') do
-      it { should_not be_enabled }
-      it { should_not be_running }
-    end
-
-    describe service('ibacm') do
-      it { should_not be_enabled }
-      it { should_not be_running }
-    end
-  end
-
-  context 'when with_optional_packages => true' do
-    it 'should run successfully' do
-      pp =<<-EOS
-        class { 'infiniband': with_optional_packages => true }
-      EOS
-
-      apply_manifest(pp, :catch_failures => true)
-      expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
-    end
-
-    [
-      'libibcm',
-      'libibverbs',
-      'libibverbs-utils',
-      'librdmacm',
-      'librdmacm-utils',
-      'rdma',
-      'dapl',
-      'ibacm',
-      'ibsim',
-      'ibutils',
-      'libcxgb3',
-      'libibmad',
-      'libibumad',
-      'libipathverbs',
-      'libmlx4',
-      'libmthca',
-      'libnes',
-      'rds-tools',
-    ].each do |pkg|
-      describe package(pkg) do
-        it { should be_installed }
-      end
-    end
-
-    [
-      'compat-dapl',
-      'infiniband-diags',
-      'libibcommon',
-      'mstflint',
-      'perftest',
-      'qperf',
-      'srptools',
-    ].each do |pkg|
-      describe package(pkg) do
         it { should be_installed }
       end
     end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -21,6 +21,7 @@ RSpec.configure do |c|
     hosts.each do |host|
       on host, puppet('module', 'install', 'puppetlabs/stdlib'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module', 'install', 'domcleal/augeasproviders'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'razorsedge/network'), { :acceptable_exit_codes => [0,1] }
     end
   end
 end
