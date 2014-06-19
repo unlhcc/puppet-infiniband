3b0766a955456327246cbddc6d0f22fbb7140f49
Trey Dockendorf <treydock@gmail.com>
2014-06-18 20:14:19 -0500

    * Use cleaner check for changes in acceptance tests * Fix LICENSE file - wget fail!

--- spec/acceptance/infiniband_spec.rb
+++ spec/acceptance/infiniband_spec.rb
@@ -8,7 +8,7 @@ describe 'infiniband class' do
       EOS
 
       apply_manifest(pp, :catch_failures => true)
-      expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+      apply_manifest(pp, :catch_changes => true)
     end
 
     [
