cd407881f0256788746e59e1fca4b208c5cf7b31
Trey Dockendorf <treydock@gmail.com>
2015-03-15 18:48:15 -0500

    * Fix unit tests * Use rspec3 syntax for calc_log_num_mtt tests

--- spec/unit/puppet/parser/functions/calc_log_num_mtt_spec.rb
+++ spec/unit/puppet/parser/functions/calc_log_num_mtt_spec.rb
@@ -1,43 +1,37 @@
 require 'spec_helper'
 
-describe Puppet::Parser::Functions.function(:calc_log_num_mtt) do
-
+describe 'the calc_log_num_mtt function' do
   let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
 
-  subject do
-    function_name = Puppet::Parser::Functions.function(:calc_log_num_mtt)
-    scope.method(function_name)
-  end
-
   it 'should exist' do
-    Puppet::Parser::Functions.function('calc_log_num_mtt').should == "function_calc_log_num_mtt"
+    expect(Puppet::Parser::Functions.function('calc_log_num_mtt')).to eq('function_calc_log_num_mtt')
   end
 
   it 'should return 23 when args [129035.57]' do
-    subject.call(['129035.57']).should == 23
+    expect(scope.function_calc_log_num_mtt(['129035.57'])).to eq(23)
   end
 
   it 'should return 22 when args [64399.75]' do
-    subject.call(['64399.75']).should == 22
+    expect(scope.function_calc_log_num_mtt(['64399.75'])).to eq(22)
   end
 
   it 'should return 25 when args [129035.57,1]' do
-    subject.call(['129035.57','1']).should == 25
+    expect(scope.function_calc_log_num_mtt(['129035.57','1'])).to eq(25)
   end
 
   it 'should return 24 when args [64399.75,1]' do
-    subject.call(['64399.75','1']).should == 24
+    expect(scope.function_calc_log_num_mtt(['64399.75','1'])).to eq(24)
   end
 
   it 'should handle [undef,1]' do
-    subject.call([nil,'1']).should == 0
+    expect(scope.function_calc_log_num_mtt([nil,'1'])).to eq(0)
   end
 
   it 'should raise a ParseError if there is less than 1 argument' do
-    expect { subject.call([]).to raise_error(Puppet::ParseError, /Takes at least one args, but 0 given./) }
+    expect { scope.function_calc_log_num_mtt([]).to raise_error(Puppet::ParseError, /Takes at least one args, but 0 given./) }
   end
   
   it 'should raise a ParseError if there is more than 3 argument' do
-    expect { subject.call(['1','2','3','4']).to raise_error(Puppet::ParseError, /Takes at most three args, but 4 given./) }
+    expect { scope.function_calc_log_num_mtt(['1','2','3','4']).to raise_error(Puppet::ParseError, /Takes at most three args, but 4 given./) }
   end
 end
