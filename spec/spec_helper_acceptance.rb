f320136c1cfb56aa3a7ee0eb77feddd272b4ddc9
Trey Dockendorf <tdockendorf@osc.edu>
2017-09-04 15:36:56 -0400

    Update acceptance testing

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,30 +1,15 @@
 require 'beaker-rspec'
+require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
 
-hosts.each do |host|
-  # Install Puppet
-  if host['platform'] =~ /el-(5|6)/
-    relver = $1
-    on host, "rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-#{relver}.noarch.rpm", { :acceptable_exit_codes => [0,1] }
-    on host, 'yum install -y puppet puppet-server', { :acceptable_exit_codes => [0,1] }
-  end
-end
+dir = File.expand_path(File.dirname(__FILE__))
+Dir["#{dir}/acceptance/shared_examples/**/*.rb"].sort.each {|f| require f}
+
+run_puppet_install_helper
+install_module_on(hosts)
+install_module_dependencies_on(hosts)
 
 RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
   # Readable test descriptions
   c.formatter = :documentation
-
-  # Configure all nodes in nodeset
-  c.before :suite do
-    # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'infiniband')
-
-    hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs/stdlib'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'domcleal/augeasproviders'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'razorsedge/network'), { :acceptable_exit_codes => [0,1] }
-    end
-  end
 end
